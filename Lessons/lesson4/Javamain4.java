4bd1bb527684f0ad629cc19f767c10fdb50a7fcb
AlexDI <alexd737@gmail.com>
2017-04-20 20:25:07 +0300

    Replace content

---
+++ Lessons/lesson4/Javamain4.java
@@ -0,0 +1,20 @@
+public class Javamain4{
+ public static void main(String[] args){
+
+    static
+    static double d;
+    static float f = 1.0f;
+    static long l = 1l;
+
+    System.out.println("Wow!");
+
+    System.out.println(a);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(f);
+    System.out.println(l);
+
+
+ }
+
+}
